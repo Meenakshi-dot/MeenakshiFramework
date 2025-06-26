5eabdca80c77af3deee402dbb7b64ea5693036a6
Rasikala123 <rasikalareddy9741@gmail.com>
2025-06-26 21:02:40 +0530

    Update ToCreateContactTest.java

--- MeenakshiFramework/src/test/java/contactTests/ToCreateContactTest.java
+++ MeenakshiFramework/src/test/java/contactTests/ToCreateContactTest.java
@@ -5,6 +5,7 @@ public class ToCreateContactTest {
 	public static void main(String[] args) {
 
 		System.out.println("Contact is created By meenakshi");
+		System.out.println(" Organization created By meenakshi and pulled by RAsikala");
 	}
 
 }
